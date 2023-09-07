2ddc2d0e6ab76b4f1196269a84d639b6eba0cc6d
Frant24 <36748891+Frant24@users.noreply.github.com>
2023-09-07 03:37:39 +0000

    Main Upload

--- main.cs
+++ main.cs
@@ -1,7 +1,20 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    private const double CM_PER_INCH = 2.54;
+
+    static void Main()
+    {
+        Console.WriteLine("Enter the measurement in inches:");
+        double inches = double.Parse(Console.ReadLine());
+        
+        double centimeters = ConvertInchesToCentimeters(inches);
+        Console.WriteLine($"{inches} inches is {centimeters} centimeters.");
+    }
+
+    static double ConvertInchesToCentimeters(double inches)
+    {
+        return inches * CM_PER_INCH;
+    }
 }
